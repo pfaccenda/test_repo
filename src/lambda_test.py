6db7c6d8c73672da635ca55f4341a0651c07d336
pfaccenda <pfaccenda@gmail.com>
2018-03-27 21:03:12 -0400

    update

--- src/lambda_test.py
+++ src/lambda_test.py
@@ -1,9 +1,11 @@
 import json
 import logging
+import boto3
 
 
 def lambda_handler(event, context):
-    print "--> upefrdslambda:version=12 "
+    print "--> upefrdslambda 15"
+
     print "----- begin ----------\n"
     eventsource = event['Records'][0]['EventSource']
     print "event source:", eventsource, type(eventsource)
@@ -32,12 +34,17 @@ def lambda_handler(event, context):
     print "### ALARM DATA:", snsinfo['Subject'], "from", snsinfo['TopicArn'], "###"
     print "THIS SHOULD BE SENT TO THE CLOUDOPS SNS TOPIC"
 
+    modify_dbinstance_monitoring_interval(dbinstance)
+    modify_dbinstance_allocated_storage(dbinstance)
+    describe_dbinstance(dbinstance)
+
+    print "--> upefrdslambda:version=15 "
     print "------ end ----------\n"
 
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    logger.info('got event{}'.format(event))
-    logger.info(message)
+    # logger = logging.getLogger()
+    # logger.setLevel(logging.INFO)
+    # logger.info('got event{}'.format(event))
+    # logger.info(message)
 
     return message
 
@@ -45,4 +52,68 @@ def lambda_handler(event, context):
 def my_logging_handler(event, context):
     logger.info('got event{}'.format(event))
     logger.error('something went wrong')
-    return 'Hello from Lambda!'  
+    return 'Hello from Lambda!'
+
+
+def modify_dbinstance_monitoring_interval(dbname):
+    client = boto3.client('rds')
+
+    response = client.describe_db_instances(DBInstanceIdentifier=dbname)
+    d = response['DBInstances'][0]
+    print "Original value of MonitoringInterval for", dbname, "is", d.get('MonitoringInterval')
+
+    response = client.modify_db_instance(DBInstanceIdentifier=dbname,
+                                         MonitoringInterval=30)
+
+    response = client.describe_db_instances(DBInstanceIdentifier=dbname)
+
+    print "------------------------- ", "modify_dbinstance_monitoring_interval", dbname, "------------------------- "
+    d = response['DBInstances']
+    for index in range(len(response['DBInstances'])):
+        d = response['DBInstances'][index]
+        print "MonitoringInterval for ", dbname, "changed to", d.get('MonitoringInterval')
+    print "------------------------- ", "modify_dbinstance_monitoring_interval", dbname, "------------------------- "
+
+    print ""
+
+
+def modify_dbinstance_allocated_storage(dbname):
+    client = boto3.client('rds')
+
+    response = client.describe_db_instances(DBInstanceIdentifier=dbname)
+    d = response['DBInstances'][0]
+    original_allocated_storage = d.get('AllocatedStorage')
+    print "Original value of AllocatedStorage for", dbname, "is", original_allocated_storage
+
+    new_allocated_storage = original_allocated_storage + (original_allocated_storage + 0)
+    response = client.modify_db_instance(DBInstanceIdentifier=dbname,
+                                         AllocatedStorage=new_allocated_storage,
+                                         ApplyImmediately=True)
+
+    response = client.describe_db_instances(DBInstanceIdentifier=dbname)
+
+    print "------------------------- ", "modify_dbinstance_allocated_storage", dbname, "------------------------- "
+    d = response['DBInstances']
+    for index in range(len(response['DBInstances'])):
+        d = response['DBInstances'][index]
+        print "AllocatedStorage for ", dbname, "changed to", d.get('AllocatedStorage')
+    print "------------------------- ", "modify_dbinstance_allocated_storage", dbname, "------------------------- "
+
+    print ""
+
+
+def describe_dbinstance(dbname):
+    client = boto3.client('rds')
+    response = client.describe_db_instances(DBInstanceIdentifier=dbname)
+    print "------------------------- ", "describe_dbinstance", dbname, "------------------------- "
+    d = response
+    for k, v in d.items():
+        print  k, "=", v
+        for dd in d['DBInstances']:
+            for k, v in dd.items():
+                print k, "=", v
+
+    print "------------------------- ", "describe_dbinstance", dbname, "------------------------- "
+    print ""
+
+
