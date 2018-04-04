62cd04a3d5422f77ad45d9a715e1eab5c7b376fb
pfaccenda <pfaccenda@gmail.com>
2018-04-04 05:53:30 -0400

    interim

--- src/hello.py
+++ src/hello.py
@@ -1,5 +1,8 @@
 import json
 import pprint
+import boto3
+import botocore
+
 
 def parse_event_url():
     s="http://docs.amazonwebservices.com/AmazonRDS/latest/UserGuide/USER_Events.html#RDS-EVENT-0078"
@@ -54,11 +57,70 @@ def demo_event_2():
     dimensions=message['Trigger']['Dimensions'][0]
     print(  dimensions['name'], "=", dimensions['value'])
 
+def demo_event_3():
+    event={u'Records': [{u'EventVersion': u'1.0', u'EventSubscriptionArn': u'arn:aws:sns:us-east-1:546771319769:UpefSnsTopic:79788c7b-3c2e-40ff-92af-8c02dbe04743', u'EventSource': u'aws:sns', u'Sns': {u'SignatureVersion': u'1', u'Timestamp': u'2018-04-04T09:43:06.025Z', u'Signature': u'IG74fJfeyGNF3WwWTxZJ1AjVYBJAPg4D/7lDkWtgZb/v/YLntDYWvLyVBGruhYu/Ahrv7dnhH1kUEtjQ7Yv//vjhQP42r/BfRtLDkZuF3Sb14P6LTqtGmOq6Lwxudx/mUs1HCWOlYtx2ES6r+qEk2PqWe3OVy/bbQQS0/rHsbXgqVuaGUR5PW3A+GQTupjdzxhTy5zMoQXcg7wamHj2ZMbNt7uNn6dj9N4MlnCyAZjHbgzf1muWhAiqQfA5mT2rWMbha1osvxSWXMYJwSYjYhoEAk+qlYx/lyezHCptr8KrZgE77lSErLQZr5ixd7dDPPoEEZ2SlTh9oSKBlBzS3aA==', u'SigningCertUrl': u'https://sns.us-east-1.amazonaws.com/SimpleNotificationService-433026a4050d206028891664da859041.pem', u'MessageId': u'b25ab905-aeaf-5bfa-baa9-ea3b0ec50da3', u'Message': u'{"Event Source":"db-instance","Event Time":"2018-04-04 09:40:10.307","Identifier Link":"https://console.aws.amazon.com/rds/home?region=us-east-1#dbinstance:id=upefrdsdb02","Source ID":"upefrdsdb02","Event ID":"http://docs.amazonwebservices.com/AmazonRDS/latest/UserGuide/USER_Events.html#RDS-EVENT-0005","Event Message":"DB instance created"}', u'MessageAttributes': {}, u'Type': u'Notification', u'UnsubscribeUrl': u'https://sns.us-east-1.amazonaws.com/?Action=Unsubscribe&SubscriptionArn=arn:aws:sns:us-east-1:546771319769:UpefSnsTopic:79788c7b-3c2e-40ff-92af-8c02dbe04743', u'TopicArn': u'arn:aws:sns:us-east-1:546771319769:UpefSnsTopic', u'Subject': u'RDS Notification Message'}}]}
+    pprint.pprint(event, indent=2)
+
+    print_dict( event, "event")
+    print_dict( event['Records'][0], "event ---> Records['0']")
+    print_dict(event['Records'][0]['Sns'], "event ---> ['Records'][0]['Sns']")
+
+    smessage = event['Records'][0]['Sns']['Message']
+    print type(smessage)
+    print smessage
+    message=json.loads(smessage)
+    print_dict( message, "event ---> ['Records'][0]['Sns']['Message']")
+    print message['Source ID']
+
+def write_text_to_s3_bucket(client, more_binary_data):
+    client.put_object(Body=more_binary_data, Bucket='upefbucket', Key='mydata/filename.txt')
+
+def does_bucket_exist( s3, bucket_name='' ):
+    bucket = s3.Bucket(bucket_name)
+    exists = True
+    try:
+        s3.meta.client.head_bucket(Bucket=bucket_name)
+    except botocore.exceptions.ClientError as e:
+        # If a client error is thrown, then check that it was a 404 error.
+        # If it was a 404 error, then the bucket does not exist.
+        error_code = int(e.response['Error']['Code'])
+        if error_code == 404:
+            exists = False
+    finally:
+        return exists
+
+    s3 = boto3.resource('s3')
+    if does_bucket_exist(s3, 'upefbucket') == True:
+        print "upefbucket exists"
+    else:
+        print "upefbucket does not exist"
+
+def s3_bucket_stuff():
+    # Text to Binary
+    message = "this was written by " + "upef"  # str
+    binary_message = message.encode('utf-8')
+    print(type(binary_message))  # bytes
+    client = boto3.client('s3')
+    write_text_to_s3_bucket(client, binary_message)
+
+    message = "this was written by " + "upef --------- 2"  # str
+    binary_message = message.encode('utf-8')
+    print(type(binary_message))  # bytes
+    client = boto3.client('s3')
+    write_text_to_s3_bucket(client, binary_message)
+
+
+# ------------------------------------------------------------------------
+
 def main():
     print "Hello"
     parse_event_url()
     demo_event_1()
     demo_event_2()
+    demo_event_3()
+
+    # s3_bucket_stuff()
+
 
 if __name__ == "__main__":
     main()
